9545fd0a8da912e174b48df34f191eb82e4ca8c9
Sharon Hazan <95042097+Sharonhazan@users.noreply.github.com>
2026-04-02 11:49:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,24 @@
-const CACHE_NAME = 'artrace-v1';
+const CACHE_NAME = 'artrace-v2';
 const ASSETS = [
     './',
     './index.html',
     './style.css',
     './app.js',
-    './manifest.json'
+    './manifest.json',
+    './icon-192.svg',
+    './icon-512.svg'
 ];
 
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
 });
 
 self.addEventListener('fetch', (e) => {
